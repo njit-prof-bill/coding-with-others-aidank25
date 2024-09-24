79e1389bfd3b35f88f079c1c1274bea79fd94c3d
Aidan Krenz <ark25@njit.edu>
2024-09-23 22:22:23 -0400

    implemented longest_substring_k_unique()

--- solution.py
+++ solution.py
@@ -1,3 +1,19 @@
+def setMax(max: int, new: int):
+    if (new > max):
+        return new
+    else:
+        return max
+
 def longest_substring_k_unique(s: str, k: int) -> int:
-    # Write your code here
-    pass
+    max = 0
+    for x in range(len(s)):
+        count = 0
+        charset = set()
+        for y in range(x,len(s)):
+            charset.add(s[y])
+            if len(charset) > k:
+                break
+            count += 1
+            print(count, len(charset))
+        max = setMax(max,count)    
+    return max
